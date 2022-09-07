489b39f15defea1b114a0da394766ff4b2ac4e8d
69Ali <113003602+69Ali@users.noreply.github.com>
2022-09-06 22:41:12 -0700

    Create students_result.py
    first commit

---
+++ students_result.py
@@ -0,0 +1,8 @@
+students_of_class=["Ali","UZAIR","Hashim"]
+name=input("what is your name?\n")
+print(name)
+passing_marks=int(input("how much numbers you get?\n"))
+if passing_marks >= 33:
+    print("congrats you are passed in your exam")
+else:
+    print("you are failled in your exam")    
